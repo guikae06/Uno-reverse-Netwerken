b7a7c8dc823b0a308647b9445248282241cb4480
Guikae06 <kaelig.guillemette@gmail.com>
2025-05-17 21:06:01 +0200

    het toevoegen van pthread

--- unoreverse_server.c
+++ unoreverse_server.c
@@ -5,46 +5,42 @@
 #include <arpa/inet.h>
 #include <time.h>
 #include <curl/curl.h>
+#include <pthread.h>
 
 #define LUISTER_POORT 22
 #define BUFFER_GROOTTE 1024
 #define LOGBOEK_BESTAND "unoreverse_log.txt"
 #define WHOIS_LOGBOEK "whois_log.txt"
 
-// Callback voor CURL om data in een string te stoppen
+pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Linked list voor IP's
+typedef struct IPNode {
+    char ip[INET_ADDRSTRLEN];
+    struct IPNode *volgende;
+} IPNode;
+
+IPNode *ip_lijst = NULL;
+
+// Voeg IP toe aan linked list
+void voeg_ip_toe(const char *ip) {
+    IPNode *nieuw = malloc(sizeof(IPNode));
+    if (!nieuw) return;
+    strncpy(nieuw->ip, ip, sizeof(nieuw->ip));
+    nieuw->volgende = ip_lijst;
+    ip_lijst = nieuw;
+}
+
+// Callback voor CURL
 size_t curl_schrijf_callback(void *gegevens, size_t grootte, size_t aantal, void *doel) {
     strncat((char *)doel, (char *)gegevens, grootte * aantal);
     return grootte * aantal;
 }
 
-// Log IP, ontvangen data, geolocatie en verzonden bytes
-void schrijf_logboek(const char *ip_adres, const char *ontvangen_bericht, const char *geolocatie_info, int verzonden_bytes) {
-    FILE *logboek = fopen(LOGBOEK_BESTAND, "a");
-    if (!logboek) {
-        perror("Kan logboek niet openen");
-        return;
-    }
-
-    time_t nu = time(NULL);
-    char *tijdstip = ctime(&nu);
-    tijdstip[strcspn(tijdstip, "\n")] = 0; // verwijder newline
-
-    fprintf(logboek, "[%s] IP: %s\n", tijdstip, ip_adres);
-    fprintf(logboek, "Ontvangen gegevens: %s\n", ontvangen_bericht);
-    fprintf(logboek, "Geolocatie: %s\n", geolocatie_info);
-    fprintf(logboek, "Totaal bytes teruggestuurd: %d\n\n", verzonden_bytes);
-
-    fflush(logboek);
-    fclose(logboek);
-
-    printf("Succesvol gelogd voor IP %s\n", ip_adres);
-}
-
-// Vraag geolocatie op via ip-api.com
+// Geolocatie ophalen
 void haal_geolocatie_op(const char *ip_adres, char *resultaat, size_t resultaat_grootte) {
     CURL *curl = curl_easy_init();
     if (!curl) {
-        fprintf(stderr, "CURL initialisatie mislukt\n");
         snprintf(resultaat, resultaat_grootte, "Geen geolocatie beschikbaar");
         return;
     }
@@ -57,127 +53,133 @@ void haal_geolocatie_op(const char *ip_adres, char *resultaat, size_t resultaat_
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, resultaat);
     curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
 
-    CURLcode status = curl_easy_perform(curl);
-    if (status != CURLE_OK) {
-        fprintf(stderr, "CURL fout: %s\n", curl_easy_strerror(status));
+    if (curl_easy_perform(curl) != CURLE_OK) {
         snprintf(resultaat, resultaat_grootte, "Geolocatie fout");
     }
 
     curl_easy_cleanup(curl);
 }
 
-// Voer een WHOIS-opzoeking uit en log het resultaat
+// WHOIS loggen
 void log_whois_data_van_ip(const char *ip_adres) {
-    char commando[256];
-    char leesbuffer[4096];
-    FILE *whois_output;
+    char commando[256], buffer[4096];
+    snprintf(commando, sizeof(commando), "whois %s", ip_adres);
+    FILE *whois_output = popen(commando, "r");
     FILE *logboek = fopen(WHOIS_LOGBOEK, "a");
 
-    if (!logboek) {
-        perror("Kan WHOIS logboek niet openen");
-        return;
-    }
+    if (!whois_output || !logboek) return;
 
-    snprintf(commando, sizeof(commando), "whois %s", ip_adres);
-    whois_output = popen(commando, "r");
-
-    if (!whois_output) {
-        fprintf(logboek, "WHOIS mislukt voor %s\n", ip_adres);
-        fclose(logboek);
-        return;
-    }
-
+    pthread_mutex_lock(&log_mutex);
     fprintf(logboek, "WHOIS voor IP: %s\n", ip_adres);
-    while (fgets(leesbuffer, sizeof(leesbuffer), whois_output) != NULL) {
-        fputs(leesbuffer, logboek);
+    while (fgets(buffer, sizeof(buffer), whois_output)) {
+        fputs(buffer, logboek);
     }
-    fprintf(logboek, "\n-----------------------------------\n\n");
-
-    fflush(logboek);
-    pclose(whois_output);
+    fprintf(logboek, "\n-------------------------------\n\n");
     fclose(logboek);
+    pthread_mutex_unlock(&log_mutex);
+    pclose(whois_output);
+}
 
-    printf("WHOIS info gelogd voor IP %s\n", ip_adres);
+// Logboek bijwerken
+void schrijf_logboek(const char *ip, const char *bericht, const char *geo, int verzonden) {
+    FILE *logboek = fopen(LOGBOEK_BESTAND, "a");
+    if (!logboek) return;
+
+    time_t nu = time(NULL);
+    char *tijd = ctime(&nu);
+    tijd[strcspn(tijd, "\n")] = 0;
+
+    pthread_mutex_lock(&log_mutex);
+    fprintf(logboek, "[%s] IP: %s\n", tijd, ip);
+    fprintf(logboek, "Ontvangen: %s\n", bericht);
+    fprintf(logboek, "Geolocatie: %s\n", geo);
+    fprintf(logboek, "Verzonden bytes: %d\n\n", verzonden);
+    fclose(logboek);
+    pthread_mutex_unlock(&log_mutex);
+}
+
+// Client-handler functie
+void *verwerk_client(void *arg) {
+    int client_socket = *(int *)arg;
+    free(arg);
+    struct sockaddr_in client_addr;
+    socklen_t len = sizeof(client_addr);
+    getpeername(client_socket, (struct sockaddr *)&client_addr, &len);
+
+    char ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client_addr.sin_addr, ip, sizeof(ip));
+    voeg_ip_toe(ip);
+
+    char buffer[BUFFER_GROOTTE];
+    char geo[4096] = {0};
+    char dummy_data[1024] = {0};
+    int bytes_ontvangen = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+    if (bytes_ontvangen <= 0) {
+        close(client_socket);
+        return NULL;
+    }
+    buffer[bytes_ontvangen] = '\0';
+
+    haal_geolocatie_op(ip, geo, sizeof(geo));
+    log_whois_data_van_ip(ip);
+
+    int totaal = 0;
+    while (1) {
+        int verzonden = send(client_socket, dummy_data, sizeof(dummy_data), 0);
+        if (verzonden <= 0) break;
+        totaal += verzonden;
+    }
+
+    schrijf_logboek(ip, buffer, geo, totaal);
+    close(client_socket);
+    return NULL;
 }
 
 int main() {
-    int server_socket, client_socket;
-    struct sockaddr_in server_adres, client_adres;
-    socklen_t client_adres_grootte = sizeof(client_adres);
-    char ontvangen_buffer[BUFFER_GROOTTE];
-    char client_ip[INET_ADDRSTRLEN];
-    char geolocatie_resultaat[4096] = {0};
-    char terugstuur_buffer[1024] = {0};
-    int ontvangen_bytes;
-    int totaal_verzonden_bytes;
+    int server_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
 
-    // Maak socket aan
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Socket aanmaken mislukt");
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket");
         exit(EXIT_FAILURE);
     }
 
-    // Bind socket
-    server_adres.sin_family = AF_INET;
-    server_adres.sin_addr.s_addr = INADDR_ANY;
-    server_adres.sin_port = htons(LUISTER_POORT);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(LUISTER_POORT);
 
-    if (bind(server_socket, (struct sockaddr *)&server_adres, sizeof(server_adres)) < 0) {
-        perror("Bind mislukt (poort 22 vereist rootrechten)");
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind (poort 22 vereist root)");
         close(server_socket);
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_socket, 3) < 0) {
-        perror("Luisteren mislukt");
+    if (listen(server_socket, 10) < 0) {
+        perror("Listen");
         close(server_socket);
         exit(EXIT_FAILURE);
     }
 
-    printf("UnoReverse server luistert op poort %d...\n", LUISTER_POORT);
+    printf("UnoReverse draait op poort %d\n", LUISTER_POORT);
 
     while (1) {
-        client_socket = accept(server_socket, (struct sockaddr *)&client_adres, &client_adres_grootte);
-        if (client_socket < 0) {
-            perror("Verbinding accepteren mislukt");
+        int *nieuw_client_socket = malloc(sizeof(int));
+        *nieuw_client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
+        if (*nieuw_client_socket < 0) {
+            perror("Accept");
+            free(nieuw_client_socket);
             continue;
         }
 
-        inet_ntop(AF_INET, &client_adres.sin_addr, client_ip, sizeof(client_ip));
-        printf("Nieuwe verbinding van %s\n", client_ip);
-
-        // Lees inloggegevens of eerste bericht
-        ontvangen_bytes = recv(client_socket, ontvangen_buffer, sizeof(ontvangen_buffer) - 1, 0);
-        if (ontvangen_bytes <= 0) {
-            close(client_socket);
-            continue;
-        }
-        ontvangen_buffer[ontvangen_bytes] = '\0';
-        printf("Ontvangen: %s\n", ontvangen_buffer);
-
-        // Geolocatie opvragen
-        memset(geolocatie_resultaat, 0, sizeof(geolocatie_resultaat));
-        haal_geolocatie_op(client_ip, geolocatie_resultaat, sizeof(geolocatie_resultaat));
-
-        // WHOIS data loggen
-        log_whois_data_van_ip(client_ip);
-
-        // Reverse attack: stuur oneindig data totdat client verbreekt
-        totaal_verzonden_bytes = 0;
-        while (1) {
-            int verzonden = send(client_socket, terugstuur_buffer, sizeof(terugstuur_buffer), 0);
-            if (verzonden <= 0) {
-                break;
-            }
-            totaal_verzonden_bytes += verzonden;
-        }
-
-        // Log alles
-        schrijf_logboek(client_ip, ontvangen_buffer, geolocatie_resultaat, totaal_verzonden_bytes);
-
-        close(client_socket);
+        pthread_t tid;
+        pthread_create(&tid, NULL, verwerk_client, nieuw_client_socket);
+        pthread_detach(tid); // We hoeven geen join
     }
 
     close(server_socket);
+    curl_global_cleanup();
     return 0;
 }
